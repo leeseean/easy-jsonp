8c93418439ef47533ed1d2e52622ee7a95c19658
leeseean chiu <leeseean@qq.com>
2018-09-09 02:38:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const jsonp = ({
+  url = '',
+  params = {},
+  timeout = 3000,
+  callback = 'jsonpCallback'
+}) => {
+  //拼接url
+  if (url.indexOf('?') !== -1) {
+    url = url + '&';
+  } else {
+    url = url + '?';
+  }
+  let paramsToString = '';
+  for (let key in params) {
+    paramsToString += `&${key}=${encodeURIComponent(params[key])}`;
+  }
+  paramsToString = paramsToString.slice(1);
+  url += paramsToString;
+  url += `&callback=${callback}`;
+  //url处理结束
+  let timer; //timeout处理
+  // 发起请求
+  const script = document.createElement('script');
+  script.src = url;
+  document.head.appendChild(script);
+  //发起请求结束
+
+  function cleanup() {
+    timer && clearTimeout(timer);
+    window[callback] = undefined;
+    document.head.removeChild(script);
+  }
+
+  return new Promise((resolve, reject) => {
+    //timeout处理
+    if (timeout) {
+      timer = setTimeout(() => {
+        reject(new Error('Timeout'));
+        cleanup();
+      }, timeout);
+      return;
+    }
+    //正常处理
+    window[callback] = (data) => {
+      resolve(data);
+      cleanup();
+    };
+  });
+};
+
+export default jsonp;
