f48c5046bb12cbd6aab67f2d340a0fb79155ac69
19053139 <19053139@cnsuning.com>
2020-06-08 14:03:26 +0800

    增加类型文件

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,18 @@
  * @date 2020/6/8 11:28
 
  */
-declare module 'easy-jsonp' {
+export declare interface Options {
+    url: string,
+    params?: any,
+    timeout?: number
+}
+
+export declare function jsonp(options: Options): Promise<any>
+
+declare module "easy-jsonp" {
+    export = jsonp
+}
+/*declare module 'easy-jsonp' {
 
     // 添加插件中t添加的函数，类型
     // 注意，还可以使用unexport删除核心库中已经导出的名字
@@ -19,4 +30,4 @@ declare module 'easy-jsonp' {
         params?: any,
         timeout?: number
     }
-}
+}*/
